7c1c84a035eede763bb34c344c262afffb7d44ab
Wesley Aspinall <wra427@gmail.com>
2016-10-07 07:18:46 -0400

    added default image, keyframe animations, updated timeSince

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
+'use-strict';
+
 (function() {
 
 
   $('.date').each(function() {
     var postDate = $(this).html();
-    var processedDate = moment().diff(postDate, 'hours');
-    var dur = moment.duration(processedDate, "hours").humanize();
+    var processedDate = moment().diff(postDate, 'seconds');
+    var duration = moment.duration(processedDate, "seconds").humanize();
 
-    var timeSince = 'posted ' + dur + ' ago';
+    var timeSince = 'posted ' + duration + ' ago';
+    
     return $(this).html(timeSince);
   });
+
+  var content = $('.post-content').text();
+
+  console.log(content.split('').length);
 
 })();
